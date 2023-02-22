66dbc179fbe9de82c1e9d6fd15f1611749ca319d
GreekGamingDev <s5216712@bournemouth.ac.uk>
2023-02-22 02:32:15 +0000

    Core Implemented to enemies

--- Assets/_SCRIPTS/Core/Core.cs
+++ Assets/_SCRIPTS/Core/Core.cs
@@ -5,18 +5,51 @@ using UnityEngine;
 
 public class Core : MonoBehaviour
 {
-    public MovementComponent MovementComponent { get; private set; }
-    public CollisionSenses CollisionSenses { get; private set; }
+    public MovementComponent MovementComponent
+    {
+        get
+        {
+            if (_movementComponent != null)
+            {
+                return _movementComponent;
+            }
+            Debug.LogError("No Movement Core Component on" + transform.parent.name);
+            return null;
+        }
+
+        private set
+        {
+            _movementComponent = value;
+        }
+    }
+
+    public CollisionSenses CollisionSenses
+    {
+        get
+        {
+            if (_collisionSenses != null)
+            {
+                return _collisionSenses;
+            }
+            Debug.LogError("No Collision Senses Core Component on" + transform.parent.name);
+            return null;
+        }
+
+        private set
+        {
+            _collisionSenses = value;
+        }
+    }
+
+    private MovementComponent _movementComponent;
+    private CollisionSenses _collisionSenses;
 
     private void Awake()
     {
         MovementComponent = GetComponentInChildren<MovementComponent>();
         CollisionSenses = GetComponentInChildren<CollisionSenses>();
 
-        if (!MovementComponent || !CollisionSenses)
-        {
-            Debug.LogError("Missing Core Component");
-        }
+        
     }
 
     public void EveryFrameUpdate()
